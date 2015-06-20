0524ec8713ad99efe11356b378f7e27387a2fd6d
Seiya Bryant <seiyathesaint@gmail.com>
2015-06-20 16:51:00 +0900

    added faker japanese into seeds file added company information and company id into seeds file created a new model then added null to name of new model migration file migrated the new model added a new column to the new model "company"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620012300) do
+ActiveRecord::Schema.define(version: 20150620072118) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "url"
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string   "family_name"
@@ -19,6 +27,9 @@ ActiveRecord::Schema.define(version: 20150620012300) do
     t.string   "email_string"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "company_id"
   end
+
+  add_index "customers", ["company_id"], name: "index_customers_on_company_id"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Company.create(name: "スパルタ商事", url: "http://spartarails.com", address: "東京")
+Company.create(name: "英語だよ", url: "http://eigodayo.com", address: "神奈川")
+Company.create(name: "アメージング社", url: "http://amazingcompany.com", address: "大阪")
+
 100.times do |index|
 Customer.create(
-  family_name: "Bryant",
-  given_name: "Curtis",
-  email_string: "example_#{index}@test.com")
+  family_name: Faker::Japanese::Name.last_name,
+  given_name: Faker::Japanese::Name.first_name,
+  email_string: "example_#{index}@test.com",
+  company_id: rand(3) + 1
+  )
 end
