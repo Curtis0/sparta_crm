6f319ca4abbe899189b993c636d7654d72c78809
Seiya Bryant <seiyathesaint@gmail.com>
2015-06-20 10:59:36 +0900

    altered the configuration of rails for time and language also input a japanese text file for dates, times, etc.

--- db/migrate/20150620012300_create_customers.rb
+++ db/migrate/20150620012300_create_customers.rb
@@ -1,11 +1,12 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers do |t|
-      t.string :family_name
-      t.string :given_name
-      t.string :email_string
+      t.string :family_name,  null: false, length: { maximum: 20 }
+      t.string :given_name,   null: false, length: { maximum: 20 }
+      t.string :email_string, null: false
 
       t.timestamps null: false
     end
+    add_index :customers, :email_string, unique: true
   end
 end
