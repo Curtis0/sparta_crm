bc67ec2e5d1d0e492218457265a83ca61ece1d65
Seiya Bryant <seiyathesaint@gmail.com>
2015-06-27 09:55:53 +0900

    found answer to problem with showing image on show page of customer. edited application controller.rb to include the special 呪文 for the devise gem. added the code in the edit.html.erb to show the image of each customer. added the code in the new.html.er to show the field to add image url data to each customer.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :family_name
     devise_parameter_sanitizer.for(:sign_up) << :given_name
+    devise_parameter_sanitizer.for(:sign_up) << :image_url
     devise_parameter_sanitizer.for(:account_update) << :family_name
     devise_parameter_sanitizer.for(:account_update) << :given_name
+    devise_parameter_sanitizer.for(:account_update) << :image_url
   end
 
 end
